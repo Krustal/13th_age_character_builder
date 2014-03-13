6bc4c78ae7e199d49b462aa440e8664f0ceb8af8
Byron McMullen <bmcmullen@whitepages.com>
2014-03-12 19:51:54 -0700

    organized and clarified a good portion of the character controller

--- app/scripts/controllers/character.js
+++ app/scripts/controllers/character.js
@@ -2,44 +2,54 @@
 
 angular.module('13thAgeCharacterBuilderApp')
   .controller('CharacterCtrl', function ($scope) {
-    function modifierFunc(baseAttr){
-      return function(){
-        $scope.updateAttribute(baseAttr);
-      };
-    }
 
-    $scope.updateAttribute = function(attribute) {
-      var base = parseInt(attribute.value, 10),
-        level = parseInt($scope.level, 10),
-        mod;
-      // update attribute modifier
-      attribute.mod = Math.floor((base - 10) / 2);
-      mod = parseInt(attribute.mod, 10);
-      // update attribute modifier + level
-      attribute.modLvl = mod + level;
+    $scope.races = [
+      { name: 'Human'     },
+      { name: 'Dwarf'     },
+      { name: 'Dark Elf'  },
+      { name: 'High Elf'  },
+      { name: 'Wood Elf'  },
+      { name: 'Gnome'     },
+      { name: 'Half-elf'  },
+      { name: 'Halfling'  },
+      { name: 'Half-orc'  }
+    ];
+
+    $scope.classes = [
+      { name: 'Barbarian' },
+      { name: 'Bard'      },
+      { name: 'Cleric'    },
+      { name: 'Fighter'   },
+      { name: 'Paladin'   },
+      { name: 'Ranger'    },
+      { name: 'Rogue'     },
+      { name: 'Sorcerer'  },
+      { name: 'Wizard'    }
+    ];
+
+    $scope.character = {
+      name: '',
+      race: '',
+      class: '',
+      level: 1,
+      attributes: [
+        { name: 'str', value: 10 },
+        { name: 'dex', value: 10 },
+        { name: 'con', value: 10 },
+        { name: 'int', value: 10 },
+        { name: 'wis', value: 10 },
+        { name: 'cha', value: 10 }
+      ]
     };
 
-    $scope.level = 1;
-    $scope.attributes = {};
-    $scope.attributeOrder = ['str', 'con', 'dex', 'int', 'wis', 'cha'];
-    for(var i in $scope.attributeOrder) {
-      var attribute = $scope.attributeOrder[i];
-      $scope.attributes[attribute] = {};
-      var attr = $scope.attributes[attribute];
-      attr.value = 10;
+    // Determine the attribute modifier
+    $scope.attrMod = function(attr) {
+      return parseInt(Math.floor((attr - 10) / 2), 10);
+    };
 
-      // When the value of an attribute changes, update its modifier
-      $scope.$watch('attributes.' + attribute + '.value',
-        modifierFunc(attr));
-    }
-
-    $scope.$watch('level', function(){
-      for(var i in $scope.attributeOrder){
-        var attribute = $scope.attributeOrder[i];
-
-        var mod = parseInt($scope.attributes[attribute].mod, 10);
-        $scope.attributes[attribute].modLvl = mod + parseInt($scope.level, 10);
-      }
-    });
+    // Determine the attribute modifier + level
+    $scope.attrModLvl = function(attr) {
+      return $scope.attrMod(attr) + $scope.character.level;
+    };
   });
 
