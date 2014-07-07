82936cf0f1b2b8e4e61594f0d57b3eb5bafe0ba5
Byron McMullen <bmcmullen@whitepages.com>
2014-07-06 18:21:16 -0700

    point buy attributes

--- app/scripts/controllers/character.js
+++ app/scripts/controllers/character.js
@@ -2,18 +2,24 @@
 
 angular.module('13thAgeCharacterBuilderApp')
   .controller('CharacterCtrl', function ($scope, Race, Classes) {
-    $scope.attributes = ['str', 'con', 'dex', 'int', 'wis', 'cha'];
 
-    $scope.races = Race.query();
-
-    $scope.classes = Classes.query();
+    var abilityCosts = {
+      8:  0,
+      9:  1,
+      10: 2,
+      11: 3,
+      12: 4,
+      13: 5,
+      14: 6,
+      15: 8,
+      16: 10,
+      17: 13,
+      18: 16
+    };
 
     function getMod(){
       /*jshint validthis:true */
-      var mod = parseInt(Math.floor((this.value - 10) / 2), 10);
-      if(this.name === $scope.character.racialAbilityBonus || this.name === $scope.character.classAbilityBonus){
-        mod += 2;
-      }
+      var mod = parseInt(Math.floor((this.value() - 10) / 2), 10);
       return mod;
     }
 
@@ -22,6 +28,30 @@ angular.module('13thAgeCharacterBuilderApp')
       return this.mod() + parseInt($scope.character.level, 10);
     }
 
+    function getValue(){
+      /*jshint validthis:true */
+      if($scope.character && (this.name === $scope.character.racialAbilityBonus || this.name === $scope.character.classAbilityBonus)){
+        return this.baseValue + 2;
+      } else {
+        return this.baseValue;
+      }
+    }
+
+    function initializeAttrs(attributes, start) {
+      var attributeObjects = {};
+      for (var attribute in attributes) {
+        attributeObjects[attributes[attribute]] = { name: attributes[attribute], baseValue: start, value: getValue, mod: getMod, modPlusLvl: getModLvl };
+      }
+      return attributeObjects;
+    }
+
+    $scope.decrementAttr = function(attr) { attr.baseValue -= 1; };
+    $scope.incrementAttr = function(attr) { attr.baseValue += 1; };
+
+    $scope.races = Race.query();
+    $scope.classes = Classes.query();
+    $scope.attributes = ['str', 'con', 'dex', 'int', 'wis', 'cha'];
+
     $scope.character = {
       name: '',
       race: $scope.races.human,
@@ -29,14 +59,14 @@ angular.module('13thAgeCharacterBuilderApp')
       class: $scope.classes.fighter,
       classAbilityBonus: null,
       level: 1,
-      attributes: [
-        { name: 'str', value: 10, mod: getMod, modPlusLvl: getModLvl },
-        { name: 'dex', value: 10, mod: getMod, modPlusLvl: getModLvl },
-        { name: 'con', value: 10, mod: getMod, modPlusLvl: getModLvl },
-        { name: 'int', value: 10, mod: getMod, modPlusLvl: getModLvl },
-        { name: 'wis', value: 10, mod: getMod, modPlusLvl: getModLvl },
-        { name: 'cha', value: 10, mod: getMod, modPlusLvl: getModLvl }
-      ]
+      attributes: initializeAttrs($scope.attributes, 8),
+      abilityPoints: function(){
+        var points = 28;
+        for(var attribute in this.attributes) {
+          points -= abilityCosts[this.attributes[attribute].baseValue];
+        }
+        return points;
+      }
     };
   });
 
